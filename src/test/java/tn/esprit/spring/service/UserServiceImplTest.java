5c8d50b5566b2baa97ed31d3a5c8669b4d123ed4
mboudebous <126955626+mboudebous@users.noreply.github.com>
2023-06-24 18:07:36 +0100

    Update UserServiceImplTest.java

--- src/test/java/tn/esprit/spring/service/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/UserServiceImplTest.java
@@ -37,7 +37,7 @@ public class UserServiceImplTest {
 		public void testAddUser() throws ParseException {
 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			Date d = dateFormat.parse("2015-03-23");
-			User u = new User("Mayssa1", "Mayssa1", d, Role.INGENIEUR); 
+			User u = new User("Mayssa3", "Mayssa3", d, Role.INGENIEUR); 
 			User userAdded = us.addUser(u); 
 			Assert.assertEquals(u.getLastName(), userAdded.getLastName());
 		}
@@ -46,7 +46,7 @@ public class UserServiceImplTest {
 		public void testModifyUser() throws ParseException   {
 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			Date d = dateFormat.parse("2015-03-23");
-			User u = new User(5L, "Mayssa122222222", "Mayssa", d, Role.INGENIEUR); 
+			User u = new User(5L, "Maysaaa3333", "Mayssa", d, Role.INGENIEUR); 
 			User userUpdated  = us.updateUser(u); 
 			Assert.assertEquals(u.getLastName(), userUpdated.getLastName());
 		}
@@ -54,7 +54,7 @@ public class UserServiceImplTest {
 		@Test
 		public void testRetrieveUser() {
 			User userRetrieved = us.retrieveUser("1"); 
-			Assert.assertEquals(1L, userRetrieved.getId().longValue());
+			Assert.assertEquals(2L, userRetrieved.getId().longValue());
 		}
 		
 		@Test
